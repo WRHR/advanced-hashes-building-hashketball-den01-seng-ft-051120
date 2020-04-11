72f20af8e47acca10566a43c7617098c892b752a
Will Reeves <wrhreeves@gmail.com>
2020-04-11 20:17:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,9 +2,11 @@
 def game_hash
   game_hash = {
     :home => {
-      :team_name => "",
-      :colors => [],
-      :players => []
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black","White"],
+      :players => [
+        {:player_name => , :number}
+        ]
     },
     :away => {
       :team_name => "",
